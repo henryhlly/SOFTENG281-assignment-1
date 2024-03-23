790526cc7a90c58252adf1942fda8e0c8c123ef1
Henry Ly <lyhenry2004@gmail.com>
2024-03-24 00:08:16 +1300

    Implemented Venue object creation with correct parameters
    Also has appending to appropriate lists to record venue names and codes.

--- src/main/java/nz/ac/auckland/se281/Venue.java
+++ src/main/java/nz/ac/auckland/se281/Venue.java
@@ -1,5 +1,17 @@
 package nz.ac.auckland.se281;
 
 public class Venue {
+  private String venueName;
+  private String venueCode;
+  private int venueCapacity;
+  private int hireFee;
 
+  
+  public Venue(String venueName, String venueCode, int venueCapacity, int hireFee) {
+    this.venueName = venueName;
+    this.venueCode = venueCode;
+    this.venueCapacity = venueCapacity;
+    this.hireFee = hireFee;
+  }
+ 
 }
--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -17,13 +17,17 @@ public class VenueHireSystem {
 
   public void createVenue(String venueName, String venueCode, String capacityInput, String hireFeeInput) {
 
+    boolean valid = true;
+
     // Testing venueName validity
     if (venueName.trim().isEmpty()) {
       MessageCli.VENUE_NOT_CREATED_EMPTY_NAME.printMessage();
+      valid = false;
     }
     // Testing venueCode validity
     else if (venue_codes.contains(venueCode)) {
       MessageCli.VENUE_NOT_CREATED_CODE_EXISTS.printMessage(venueCode, venue_names.get(venue_codes.indexOf(venueCode)));
+      valid = false;
     }
     else { 
       // Testing venueCapacity validity
@@ -31,10 +35,12 @@ public class VenueHireSystem {
         Integer.parseInt(capacityInput);
       } catch(Exception e) {
         MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("capacity", " ");
+        valid = false;
       }
       int venueCapacity = Integer.parseInt(capacityInput);
       if (venueCapacity <= 0) {
         MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("capacity", " positive");
+        valid = false;
       }
 
       // Testing hireFee validity
@@ -42,13 +48,21 @@ public class VenueHireSystem {
         Integer.parseInt(hireFeeInput);
       } catch(Exception e) {
         MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("hire fee", " ");
+        valid = false;
       }
       int hireFee = Integer.parseInt(capacityInput);
       if (hireFee <= 0) {
         MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("hire fee", " positive");
+        valid = false;
+      }
+
+      if (valid == true) {
+        Venue venue = new Venue(venueName, venueCode, venueCapacity, hireFee);
+        venue_names.add(venueName);
+        venue_codes.add(venueCode);
       }
     }
-    
+
   }
 
   public void setSystemDate(String dateInput) {
