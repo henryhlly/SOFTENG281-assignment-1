e3a0ecd1147908581149382ccc3af840b457132a
Henry Ly <lyhenry2004@gmail.com>
2024-03-23 23:51:11 +1300

    Made statements that tested validity of parameters
    createVenue tests each parameter properly and in a specific order but has yet to create new Venue type object.

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -3,17 +3,52 @@ package nz.ac.auckland.se281;
 import nz.ac.auckland.se281.Types.CateringType;
 import nz.ac.auckland.se281.Types.FloralType;
 
+import java.util.ArrayList;
+
 public class VenueHireSystem {
+  ArrayList<String> venue_names = new ArrayList<String>();
+  ArrayList<String> venue_codes = new ArrayList<String>();
 
   public VenueHireSystem() {}
 
   public void printVenues() {
-    // TODO implement this method
+    System.out.println("There are no venues in the system. Please create a venue first.");
   }
 
-  public void createVenue(
-      String venueName, String venueCode, String capacityInput, String hireFeeInput) {
-    // TODO implement this method
+  public void createVenue(String venueName, String venueCode, String capacityInput, String hireFeeInput) {
+
+    // Testing venueName validity
+    if (venueName.trim().isEmpty()) {
+      MessageCli.VENUE_NOT_CREATED_EMPTY_NAME.printMessage();
+    }
+    // Testing venueCode validity
+    else if (venue_codes.contains(venueCode)) {
+      MessageCli.VENUE_NOT_CREATED_CODE_EXISTS.printMessage(venueCode, venue_names.get(venue_codes.indexOf(venueCode)));
+    }
+    else { 
+      // Testing venueCapacity validity
+      try {
+        Integer.parseInt(capacityInput);
+      } catch(Exception e) {
+        MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("capacity", " ");
+      }
+      int venueCapacity = Integer.parseInt(capacityInput);
+      if (venueCapacity <= 0) {
+        MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("capacity", " positive");
+      }
+
+      // Testing hireFee validity
+      try {
+        Integer.parseInt(hireFeeInput);
+      } catch(Exception e) {
+        MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("hire fee", " ");
+      }
+      int hireFee = Integer.parseInt(capacityInput);
+      if (hireFee <= 0) {
+        MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("hire fee", " positive");
+      }
+    }
+    
   }
 
   public void setSystemDate(String dateInput) {
