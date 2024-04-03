8cb190e465c53f98f8fb4f69ba3a1bb2e8c1ad1b
Henry Ly <lyhenry2004@gmail.com>
2024-04-04 11:43:07 +1300

    Setting and printing system date functioning
    Passed Test Case 1 of task 2

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -1,40 +1,45 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
 import nz.ac.auckland.se281.Types.CateringType;
 import nz.ac.auckland.se281.Types.FloralType;
 
-import java.util.ArrayList;
-
 public class VenueHireSystem {
   ArrayList<Venue> venues = new ArrayList<Venue>();
+  String systemDate = "";
 
   public VenueHireSystem() {}
 
   public void printVenues() {
     int quantity = venues.size();
-    String[] numStrings = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+    String[] numStrings = {
+      "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"
+    };
 
     // Print intital statement for all cases
-    if (quantity==0) {
+    if (quantity == 0) {
       MessageCli.NO_VENUES.printMessage();
-    }
-    else if (quantity==1) {
+    } else if (quantity == 1) {
       MessageCli.NUMBER_VENUES.printMessage("is", "one", "");
-    }
-    else if (quantity < 10) {
+    } else if (quantity < 10) {
       MessageCli.NUMBER_VENUES.printMessage("are", numStrings[quantity], "s");
-    }
-    else {
+    } else {
       MessageCli.NUMBER_VENUES.printMessage("are", String.valueOf(quantity), "s");
     }
-    
+
     // Print itemised list of all existing venues
-    for (Venue v:venues) {
-      MessageCli.VENUE_ENTRY.printMessage(v.getVenueName(), v.getVenueCode(), String.valueOf(v.getVenueCapacity()), String.valueOf(v.getHireFee()), "");
+    for (Venue v : venues) {
+      MessageCli.VENUE_ENTRY.printMessage(
+          v.getVenueName(),
+          v.getVenueCode(),
+          String.valueOf(v.getVenueCapacity()),
+          String.valueOf(v.getHireFee()),
+          "");
     }
   }
 
-  public void createVenue(String venueName, String venueCode, String capacityInput, String hireFeeInput) {
+  public void createVenue(
+      String venueName, String venueCode, String capacityInput, String hireFeeInput) {
 
     boolean valid = true;
 
@@ -43,24 +48,28 @@ public class VenueHireSystem {
       MessageCli.VENUE_NOT_CREATED_EMPTY_NAME.printMessage();
       valid = false;
     }
-    
-    //Testing venueCode validity
+
+    // Testing venueCode validity
     else {
-      for (Venue v: venues) {
+      for (Venue v : venues) {
         if (venueCode.equals(v.getVenueCode())) {
-          
+
           MessageCli.VENUE_NOT_CREATED_CODE_EXISTS.printMessage(venueCode, v.getVenueName());
           valid = false;
         }
       }
     }
-      
-    // Testing venueCapacity validity
-    if (valid) {valid = (checkPositiveInteger(capacityInput, "capacity"));}
 
-    //Testing hireFee validity
-    if (valid) {valid = (checkPositiveInteger(hireFeeInput, "hire fee"));}
-        
+    // Testing venueCapacity validity
+    if (valid) {
+      valid = (checkPositiveInteger(capacityInput, "capacity"));
+    }
+
+    // Testing hireFee validity
+    if (valid) {
+      valid = (checkPositiveInteger(hireFeeInput, "hire fee"));
+    }
+
     // Create the new Venue
     if (valid == true) {
       int venueCapacity = Integer.parseInt(capacityInput);
@@ -71,32 +80,36 @@ public class VenueHireSystem {
     }
   }
 
-  // New method for checking whether an input is a positive integer and returns boolean/displays appropriate error message if it is not
+  // New method for checking whether an input is a positive integer and returns boolean/displays
+  // appropriate error message if it is not
   public boolean checkPositiveInteger(String input, String var_name) {
     try {
       if (Integer.parseInt(input) <= 0) {
         MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage(var_name, " positive");
         return (false);
-      }
-      else {
+      } else {
         return (true);
       }
 
-    } catch(Exception e) {
+    } catch (Exception e) {
       MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage(var_name, "");
       return (false);
     }
   }
 
-  
   //// Checkpoint 2 ////
 
   public void setSystemDate(String dateInput) {
-    // TODO implement this method
+    systemDate = dateInput;
+    MessageCli.DATE_SET.printMessage(dateInput);
   }
 
   public void printSystemDate() {
-    // TODO implement this method
+    if (systemDate.isEmpty()) {
+      MessageCli.CURRENT_DATE.printMessage("not set");
+    } else {
+      MessageCli.CURRENT_DATE.printMessage(systemDate);
+    }
   }
 
   public void makeBooking(String[] options) {
